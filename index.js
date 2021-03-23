99e011549e436dcb05728e87115f88bdf2bd8ec0
jhakulinen <hakulisenjukka96@gmail.com>
2021-03-23 22:13:20 +0200

    Progress

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-var express = require("express")
-var fs = require("fs")
+var express = require("express");
+var fs = require("fs");
 var app = express();
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+const PORT = process.env.PORT || 8080;
+
+app.get("/", function (req, res){
+    var json = require(__dirname + "/exampledata.json")
+    res.render("pages/index");
+    console.log(json);
+})
+
+app.get("*", function(req, res){
+    res.send("Can't find the requested page", 404)
+});
+
+app.listen(PORT, function(){
+    console.log("Server is running!")
+});
