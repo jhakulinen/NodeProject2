c9377d73cd43ad51495662497277cdc14b38981c
jhakulinen <hakulisenjukka96@gmail.com>
2021-03-25 22:00:48 +0200

    Comments added

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ app.use(bodyParser.json());
 app.set("view engine", "ejs");
 const PORT = process.env.PORT || 8080;
 
+// Luodaan "aloitussivu".
 app.get("/", function (req, res){
+    // Lisätään paikallinen JSON-tiedosto muuttujaan.
     var json = require(__dirname + "/exampledata.json")
+    // Renderöidään index.ejs -tiedosto sivulle ja tuodaan mukana json-muuttujaan sijoitettu data.
     res.render("pages/index", json);
     
 })
@@ -16,7 +19,7 @@ app.get("/", function (req, res){
 app.get("*", function(req, res){
     res.send("Can't find the requested page", 404)
 });
-
+// Asetetaan ohjelma kuuntelemaan Herokun määrittämää porttia tai vaihtoehtoisesti porttia 8080.
 app.listen(PORT, function(){
     console.log("Server is running!")
 });
